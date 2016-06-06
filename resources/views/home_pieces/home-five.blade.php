fc05a33034d7afd7498d84b36fc4636b26acab76
wanyu-onisama <wanyu930403@gmail.com>
2016-06-06 10:15:39 +0800

    5优点韩语

--- resources/views/home_pieces/home-five.blade.php
+++ resources/views/home_pieces/home-five.blade.php
@@ -4,47 +4,46 @@
 <div class="row home-five">
     <div class="col-md-4">
         <div class="home-five-img" style="background-image: url(//img.alicdn.com/tps/TB1r6K0LFXXXXc5XpXXXXXXXXXX-277-184.png);">
-            <h2>高性价比</h2>
-            <p>相比传统IDC，</p>
-            <p>节约80%成本投入，</p>
-            <p>对比其他服务商，</p>
-            <p>你会节省60%成本投入。</p>
+            <h2>비용절감</h2>
+            <p>물리 서버 대비,</p>
+            <p>초기 투자비 80% 이상 비용절감,</p>
+            <p>다른 사업자 대비,</p>
+            <p>60% 이상 비용절감.</p>
         </div>
     </div>
     <div class="col-md-4">
         <div class="home-five-img" style="background-image: url(https://img.alicdn.com/tps/TB1s6DgLFXXXXalXXXXXXXXXXXX-277-184.png);">
-            <h2>高性价比</h2>
-            <p>相比传统IDC，</p>
-            <p>节约80%成本投入，</p>
-            <p>对比其他服务商，</p>
-            <p>你会节省60%成本投入。</p>
+            <h2>안정성</h2>
+            <p>자체개발한 저변 시스,</p>
+            <p>대용량 소토리지 보안성 보장,</p>
+            <p>다층 백업 및 신속한 장애복구,</p>
+            <p>용량이 필요한 만큼 원하는 대로 확장,</p>
+            <p>쉽고 안전하고 유연한 서비스.</p>
         </div>
     </div>
     <div class="col-md-4">
         <div class="home-five-img" style="background-image: url(//img.alicdn.com/tps/TB1OHLaLFXXXXXZXpXXXXXXXXXX-277-184.png);">
-            <h2>高性价比</h2>
-            <p>相比传统IDC，</p>
-            <p>节约80%成本投入，</p>
-            <p>对比其他服务商，</p>
-            <p>你会节省60%成本投入。</p>
+            <h2>보안성</h2>
+            <p>10년 세계적 최대 사전 침입 차단 경험,</p>
+            <p>고객이 안보 위험 감소에 도움이 된다,</p>
+            <p>클라우드 서비스를 보호하고 배려한다.</p>
         </div>
     </div>
     <div class="col-md-4">
         <div class="home-five-img" style="background-image: url(//img.alicdn.com/tps/TB1EhKRLFXXXXX3aXXXXXXXXXXX-277-184.png);">
-            <h2>高性价比</h2>
-            <p>相比传统IDC，</p>
-            <p>节约80%成本投入，</p>
-            <p>对比其他服务商，</p>
-            <p>你会节省60%成本投入。</p>
+            <h2>생태시스템</h2>
+            <p>많은 네이 인터내셔널 그룹이 모이다,</p>
+            <p>업계 수직시장이 협업하다,</p>
+            <p>애플리케이션 서비스 기술력을 축적,</p>
+            <p>기술력 수출 자원 공유.</p>
         </div>
     </div>
     <div class="col-md-4">
         <div class="home-five-img" style="background-image: url(//img.alicdn.com/tps/TB1sveULFXXXXcqXVXXXXXXXXXX-277-184.png);">
-            <h2>高性价比</h2>
-            <p>相比传统IDC，</p>
-            <p>节约80%成本投入，</p>
-            <p>对比其他服务商，</p>
-            <p>你会节省60%成本投入。</p>
+            <h2>최우수 서비스</h2>
+            <p>7*24시간  테크센터를 운영을 지원하다,</p>
+            <p>백배로 고장 배상을 약속하다,</p>
+            <p>더 안심하게 사용할 수 있다.</p>
         </div>
     </div>
 </div>
