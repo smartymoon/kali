9bfaa17c2d71091578fda9965a05172c5a72b974
wanyu-onisama <wanyu930403@gmail.com>
2016-06-14 14:54:32 +0800

    DDOS-IP

--- resources/views/product/ddos.blade.php
+++ resources/views/product/ddos.blade.php
@@ -7,12 +7,9 @@
             <div class="row">
                 <div class="col-md-12">
                         <div class="introduce back-ddos">
-                            <h1><i class="product-icons-64 product-icons-ddos"></i> some text</h1>
+                            <h1><i class="product-icons-64 product-icons-ddos"></i> Anti-DDoS IP</h1>
                             <p>
-                                some description, some description, some description,
-                                some description, some description, some description, some description, some description, some description,
-                                some description, some description, some description,
-                                some description, some description,
+                                AliCloud의 Anti-DDoS IP 서비스는 인터넷 서버(AliCloud 서버가 아닌 경우 포함)가 대규모의 DDoS 공격을 받은 후 서버를 사용할 수 없게 될 상황에 대비해 출시한 유료 서비스입니다. 사용자는 방어용 IP를 배치하고, DDoS 공격시 방어용IP 로 트래픽을 유도하여 사이트의 안정성을 보장합니다.
                             </p>
                         </div>
                 </div>
@@ -20,57 +17,102 @@
         </div>
 </section>
 <section class="ali_table">
-        <div class="container">
-        <h4>compared with Troditional server ,ecs 's advantual is obvious</h4>
+        <div class="container" style="padding-left:0px;height: 33px;margin-top: 30px;margin-bottom: 50px;border-bottom: solid 1px #ddd;">
+            <h1 style="font-size: 24px;background-color: #fff;display: inline-block;padding-right: 10px;">상품 개요</h1>
+        </div>
+        <div class="container"> 
         <table class="table table-bordered">
-            <thead>
                 <tr>
-                    <th style="width:150px"></th>
-                    <th>Ali</th>
-                    <th>Troditional</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>case</td>
+                    <td><img src="//img.alicdn.com/tps/TB1QMVOKXXXXXbwXFXXXXXXXXXX-32-32.png" width="18" height="18"> 대규모 DDoS 공격 방어</td>
                     <td>
-                        Ali spe<br>
-                        Ali spe
+                        <ul>
+                            <li>China Telecom, China Mobile, China Unicom, Education Network등 독립적인 방어를 지원</li>
+                            <li>DDoS 클러스터 1000G+방어, 온라인 케이블당 300G, 사용일수에 의한 정산지원</li>
+                            <li>높은 품질의 백본 네트워크 접속, 중국 내 AliCloud 리전 내에서는 50ms 이하의 latency 보장</li>
+                        </ul>
                     </td>
-                    <td>Tro spe</td>
                 </tr>
                 <tr>
-                    <td>case</td>
-                    <td>Ali spe</td>
-                    <td>Tro spe</td>
+                    <td><img src="//img.alicdn.com/tps/TB1t1GnKXXXXXcjXVXXXXXXXXXX-32-32.png" width="18" height="18"> 광범위한 비즈니스를 지원</td>
+                    <td>
+                        <ul>
+                            <li>TCP/UDP/HTTP/HTTPS 지원</li>
+                            <li>금융, 전자상거래, 게임, 포털, 미디어 등 각종 업무 시나리오 지원</li>
+                            <li>DDoS, CC, WAF 지원</li>
+                            <li>AliCloud 서버를 사용하는 고객과 사용하지 않는 고객 모두 이용 가능</li>
+                        </ul>
+                    </td>
                 </tr>
                 <tr>
-                    <td>case</td>
-                    <td>Ali spe</td>
-                    <td>Tro spe</td>
+                    <td><img src="//img.alicdn.com/tps/TB1t1GnKXXXXXcjXVXXXXXXXXXX-32-32.png" width="18" height="18"> HTTPS 암호화</td>
+                    <td>
+                        <ul>
+                            <li>HTTPS 업무의 DDoS, CC, WAF방어를 지원</li>
+                            <li>HTTPS 전체 링크 암호화(원근 링크도 암호화)</li>
+                            <li>HTTPS Secret Key 전용 솔루션 지원</li>
+                        </ul>
+                    </td>
                 </tr>
                 <tr>
-                    <td>case</td>
-                    <td>Ali spe</td>
-                    <td>Tro spe</td>
+                    <td><img src="//img.alicdn.com/tps/TB1bGuyKXXXXXc6XFXXXXXXXXXX-32-32.png" width="18" height="18"> 업계 최고의 방어 알고리즘</td>
+                    <td>
+                        <ul>
+                            <li>게임 중 빈 연결, 늦은 연결 및 악의적인 연결배제를 방어</li>
+                            <li>IP Reputation Data, IP+Cookie, IP+Key를 채용하여 CC공격을 방어</li>
+                            <li>글로벌 좀비 네트워크 공격</li>
+                            <li>SQL 인젝션 방어, XSS, Cross-Site 공격을 방어</li>
+                        </ul>
+                    </td>
                 </tr>
-                <tr>
-                    <td>case</td>
-                    <td>Ali spe</td>
-                    <td>Tro spe</td>
-                </tr>
-                <tr>
-                    <td>case</td>
-                    <td>Ali spe</td>
-                    <td>Tro spe</td>
-                </tr>
-                <tr>
-                    <td>case</td>
-                    <td>Ali spe</td>
-                    <td>Tro spe</td>
-                </tr>
-            </tbody>
         </table>
-</div>
+        </div>
+        <div class="container" style="padding-left:0px;height: 33px;margin-top: 30px;margin-bottom: 50px;border-bottom: solid 1px #ddd;">
+            <h1 style="font-size: 24px;background-color: #fff;display: inline-block;padding-right: 10px;">상품 개요</h1>
+        </div>
+        <div class="container">
+            <div class="row ali_function_body">
+                <div class="col-md-2">
+                    <div style="background:#f5f5f5;text-align:center;height: 50px;padding-top: 5px">대규모 DDoS <br>Laundrying 능력</div>
+                </div>
+                <div class="col-md-10">
+                    <ul>
+                        <li>1,000G+인 DDoS Laundrying 능력은 SYN Flood, ACK Flood, ICMP Flood, UDP Flood, NTP Flood, SSDP Flood, DNS Flood, HTTP Flood, CC공격을 완벽하게 방어할 수 있음</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row ali_function_body">
+                <div class="col-md-2">
+                    <div style="background:#f5f5f5;text-align:center;height: 80px;padding-top: 20px">대규모 DDoS <br>Laundrying 능력</div>
+                </div>
+                <div class="col-md-10">
+                    <ul>
+                        <li>DDoS IP는 Application Layer의 DDoS공격에 대한 방어를 실시간 제공</li>
+                        <li>반복검증, 신분인증, 인증코드 등 다양한 방법을 제공하여 정확하게 악의적인 방문과 정상적인 방문을 식별할 수 있으며 사이트유형 CC와 게임유형 CC공격도 방어</li>
+                        <li>특히, 전자상거래, 금융 홍보, 게임 발표 등 큰 이벤트의 보안예방시나리오에 적용됨</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row ali_function_body">
+                <div class="col-md-2">
+                    <div style="background:#f5f5f5;text-align:center;height: 50px;padding-top: 5px">대규모 DDoS <br>Laundrying 능력</div>
+                </div>
+                <div class="col-md-10" style="padding-top:13px">
+                    <ul>
+                        <li>DDoS IP는 HTTPS 사이트의 암호화 접근을 지원하여 정확하게 SQL 인젝션, XSS 공격, WEBSHEL 업로드 등 Web 공격 행위를 식별함</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row ali_function_body">
+                <div class="col-md-2">
+                    <div style="background:#f5f5f5;text-align:center;height: 50px;padding-top: 5px">대규모 DDoS <br>Laundrying 능력</div>
+                </div>
+                <div class="col-md-10">
+                    <ul>
+                        <li>DDoS IP는 하루 300G로 사용일수에 따라 비용을 정산하며 300G 이상일 경우 Work order 방식으로 주문 가능</li>
+                        <li>DDoS IP는 탄력적으로 DDoS와 CC방어 능력을 확장할 수 있으며, 사상 최대 규모의 DDoS 공격과 CC 공격을 방어한 사례를 보유하고 있음</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
 </section>
 @endsection
