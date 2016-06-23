6a5fdc245e91099f87cf97be13488e2634296fe7
SmartyMoon <pp787qq@gmail.com>
2016-06-23 10:48:37 +0800

    ageis complete

--- resources/views/product/aegis.blade.php
+++ resources/views/product/aegis.blade.php
@@ -7,12 +7,12 @@
             <div class="row">
                 <div class="col-md-12">
                         <div class="introduce back-aegis">
-                            <h1><i class="product-icons-64 product-icons-aegis"></i> some text</h1>
+                            <h1><i class="product-icons-64 product-icons-aegis"></i> 서버 안전(안기)</h1>
                             <p>
-                                some description, some description, some description,
-                                some description, some description, some description, some description, some description, some description,
-                                some description, some description, some description,
-                                some description, some description,
+빅 데이터 위협 정보를 기반하여 서버 안전을 보장하고，실시간 모니터링하고 대량으로 구멍이 복원하고 、웹사이트  뒷문 심도 제거 、로그인 일를 감사 및 정기의 안전 기선 순시 검사， 유지，서버 데이터 유출을 피하기 위해  깨끗한 서버의 운행 환경을 보호하고，마지막 안전 방위선을 지켜 내다.
+                            </p>
+                            <p>
+                                새로운 업그레이드:서버는 필수 안전 방위선,해커 공격 실시간 감시,다양한 서버를 원격 관리하고  AliCloud 아닌 서버도 지원하다.
                             </p>
                         </div>
                 </div>
@@ -21,56 +21,106 @@
 </section>
 <section class="ali_table">
         <div class="container">
-        <h4>compared with Troditional server ,ecs 's advantual is obvious</h4>
+        <div class="ali_function_header">
+            <h3>주요 특징</h3>
+        </div>
+        <p>서버 안전은 데이터의 마지막 안전 방위선이，방어 체계를 갖추고 싶으면，서버 안전은 필수불가결의 것이다，서버에 설치한  플러그인하고 클라우드 보호 중심의 열동을 톨해 마지막 안전 방위선을 지켜 내다.</p>
         <table class="table table-bordered">
             <thead>
                 <tr>
                     <th style="width:150px"></th>
-                    <th>Ali</th>
-                    <th>Troditional</th>
+                    <th>안기사 </th>
+                    <th> 다른 보안 프로그램</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td>case</td>
+                    <td>집중 관리</td>
                     <td>
-                        Ali spe<br>
-                        Ali spe
+                        서버에 조작 인터페이스가 없고，데이터를 클라우드에 종합하다<br>
+                        Web콘솔 인터페이스에서 안전 사고를 보거나 조작을 집중 통일적인 관리하다
                     </td>
-                    <td>Tro spe</td>
+                    <td>클라이언트 사건 처리할 때마다 단일 서버<br>
+                        로그인이 필요한 ， 복잡한 절차 
+                    </td>
                 </tr>
                 <tr>
-                    <td>case</td>
-                    <td>Ali spe</td>
-                    <td>Tro spe</td>
+                    <td>경량Agent</td>
+                    <td>Agent플러그인  표준 상태가 CPU/ 메모리 사용량이 1% 미만<br>
+                        업무 최고점 자동으로 인식하고，자원이 역치를 점용하가 촉발하다,Agent자퇴하다
+                    </td>
+                    <td>
+                        소프트웨어 클라이언트 메모리 사용량을 크고，높은 업무 시스템<br>
+                        업무 고조기에 서버의 성능에 영향이 있다
+                    </td>
                 </tr>
                 <tr>
-                    <td>case</td>
-                    <td>Ali spe</td>
-                    <td>Tro spe</td>
+                    <td>실시간 모니터링</td>
+                    <td>서버에 각종 안전한 사건 저확하게 잡을 수 있다<br>
+                        침입하고 이상 행동을  실시간 경고를 실행 하여 솔루션을 제공하다
+                    </td>
+                    <td>안전 사고 빠른 속도로 처리할 수 없다<br>
+                        위험 정체성 쉽게 발생하고，실시간 해킹을 감지할 수 없다.
+                    </td>
                 </tr>
                 <tr>
-                    <td>case</td>
-                    <td>Ali spe</td>
-                    <td>Tro spe</td>
-                </tr>
-                <tr>
-                    <td>case</td>
-                    <td>Ali spe</td>
-                    <td>Tro spe</td>
-                </tr>
-                <tr>
-                    <td>case</td>
-                    <td>Ali spe</td>
-                    <td>Tro spe</td>
-                </tr>
-                <tr>
-                    <td>case</td>
-                    <td>Ali spe</td>
-                    <td>Tro spe</td>
+                    <td>동태 방어</td>
+                    <td>클라우드 방패 실천 경험하고 가장 큰 위협 정보을  데이터를 공유하<br>
+                        실시간 해커 공격 행위를 분석하는 동시에 자동 동기화하고 통일적으로 가로막다
+                    </td>
+                    <td>클라우드 방패 실천 경험하고 가장 큰 위협 정보을  데이터를 공유하<br>
+                        실시간 해커 공격 행위를 분석하는 동시에 자동 동기화하고 통일적으로 가로막다
+                    </td>
                 </tr>
             </tbody>
         </table>
 </div>
+</section>
+<section class="ali_function">
+    <div class="container">
+            <div class="ali_function_header">
+                <h3>안기사 특징</h3>
+            </div>
+            <div class="row ali_function_body">
+                <div class="col-md-1">
+                    <div class="verticle-mode">검사와 복구&nbsp;</div>
+                </div>
+                <div class="col-md-11">
+                    <h4>다양한 비즈니스 요구 사항을 충족할 수 있는 탄력적이고 안정적인 컴퓨팅 자원을 제공합니다.</h4>
+                    <p>사용자의 니즈에 따라 CPU, 메모리, 데이터디스크, 네트워크의 구성을 자유롭게 선택할 수 있습니다. 또한 사업 확장에 따른 업그레이드 진행 시 5분 미만의 다운타임 내에서 CPU와 메모리 업그레이드를 진행할 수 있습니다. IO 최적화 구성은 높은sdfsdfasdasdfasdf IO 성능을 보장하며, 인프라 레벨의 SW와 HW를 업그레이드 합니다. 또한 고성능 SSD 스토리지를 이용하여 최대 20,000 IOPS의 성능을 보장합니다.</p>
+                    <h4>목마 제거，순수한 서버 환경을 지키다</h4>
+                    <p> 사이트 뒷문 제거 엔진을 개발하고，현지하고 클라우드 제거 체계가 있다，악성 파일 샘플을 공유하고，모든 파일 형식을 제거하고，세계적인 제거율 </p>
+                    <h4>패치 관리，클라우드 고위험 허점 한변에 대량 복구하다</h4>
+                    <p> 통용 Web 소프트웨어 구멍：통용 Web 소프트웨어 구멍 실시간으로 감시하면서 ，클라우드 방패 응급 처치 능력을 공유된다 ，공식보다 더 빠른 스스로 개발한 구멍 패치，구멍 폭발하고 공식에서 패치를 발표하지 않을 떼，허점을 복구하고 방어 요격을 도와 준다
+ Windows시스템 구멍：订阅소프트 공식 패치 소식을 구독하고，공식 패치 소식을 발포하는 동시에，전문가들이구멍의 위험하고업데이트의 필요성에 대해 분석하고，스캐너 규칙을 실시간으로 업데이트 ，영향을 받은 고객에 대한  알림과 복구 안내하다.</p>
+                    <p> 업데이트 및 복구：대량이나 단일 서버한테 업데이트 지원하고 ，업무가 안정된 후 전부 업데이트 할 수 있는 것을 확보하다。그리고 쉽게 복구하고 패치 삭제도 지원하다，업무에 대한 영향을 감소하고，안전하게 패치를 설치해다
+                    </p>
+                    <h4>안전적으로 순시 검사，서버에게 전면적 검사 </h4>
+                    <p>계좌 안전 검사： 해킹 침입후 남은 계좌가 존재 여부는지 심도 검사하고 ，对그림자 계좌、숨긴 계좌、복제 계좌한테 알람
+약한 구령 검사：자주 쓰는 약한 구령 사전을 모아다，SSH、RDP、sqlserver、mysql、ftp등 서비스에 약한 암호 사용 여부 검사하다</p>
+                    <p>
+                     설정위험 검사：로그인 설정、프로세스 설정、파일 권한 설정、레지스트리 설정한테 검사，기업 서버에 접근 안전 기준이 된다
+                    </p>
+                </div>
+            </div>
+            <div class="row ali_function_body">
+                <div class="col-md-1">
+                    <div class="verticle-mode">방화벽&nbsp;</div>
+                </div>
+                <div class="col-md-11">
+                    <h4>HTTP접근 제어，자체 정의 Web공격을 차단하다</h4>
+                    <p>자체 정의 HTTP방문한 전략을 작사하다，흔한HTTP 필드（예URL、Referer、UA등）에 조건 조합을 진행하다， 전속 업무 장면의 보호 방안을 제조하다。현재  CentOS 6 시스템만  지원한다,더 많은 시스템을 지지하기 기대해 주세요. </p>
+                </div>
+            </div>
+            <div class="row ali_function_body">
+                <div class="col-md-1">
+                    <div class="verticle-mode">안전운영</div>
+                </div>
+                <div class="col-md-11">
+                    <h4>콘솔에 대량으로 스크립트 배포하고，대용량 서버를 쉽게 관리하</h4>
+                    <p>분산 기업 버전Agent로 원격 스크립트를 실행하고，운행 결과가 안기사의 클라우드 관리 센터에 되돌아오다.안기사의 클라우드 관리 센터에서 모든 서버를 관리할 수 있다，Linux서버에 대한 shell스크립트를 지원하고，对于Windows시스텀에 대한 dos명령을 지원하다.</p>
+                </div>
+            </div>
+    </div>
 </section>
 @endsection
