2e234f913a62b5990753e54689c5a94e3ec85942
yun2 <pp787qq@gmail.com>
2017-04-09 23:19:56 -0400

    add deploy.php

---
+++ deploy.php
@@ -0,0 +1,42 @@
+<?php
+namespace Deployer;
+require 'recipe/laravel.php';
+
+// Configuration
+
+set('ssh_type', 'native');
+set('ssh_multiplexing', true);
+
+set('repository', 'https://github.com/smartymoon/kali.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+
+add('writable_dirs', []);
+
+// Servers
+
+server('production', 'erp.tt12t.com')
+    ->user('lee')
+    ->password(null)
+    ->set('deploy_path', '/var/www/dkali');
+
+
+// Tasks
+
+desc('Restart PHP-FPM service');
+/*
+task('php-fpm:restart', function () {
+    // The user must have rights for restart service
+    // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
+    run('sudo systemctl restart php-fpm.service');
+});
+*/
+after('deploy:symlink', 'php-fpm:restart');
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
